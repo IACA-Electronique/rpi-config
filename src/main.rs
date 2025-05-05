47251fa9d428aa67a53994e4201001adfb749207
Julien Faure <julien.faure@iaca-electronique.com>
2025-05-05 16:01:45 +0200

    Add some implementation for test

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-pub mod config_file;
+mod config_file;
+mod file_writer;
 
 fn main() {
+    let writer = file_writer::FileWriter::new("toto.txt");
     let mut file = config_file::ConfigFile::new();
     file.load("[test_section]\nexisting_key = existing_value\n");
 
@@ -21,5 +23,10 @@ fn main() {
     match file.get("all", "dtoverlay") {
         Some(value) => println!("{}", value),
         None => println!("No value found"),
+    }
+
+    match writer.write(&file.to_string()) {
+        Ok(_) => println!("File written"),
+        Err(msg) => println!("Error: {}", msg)       
     }
 }
