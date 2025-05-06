cdd6b3141ceec541697b424ae059c948e78fd04d
Julien Faure <julien.faure@iaca-electronique.com>
2025-05-06 15:01:27 +0200

    Refactor FileSystemManager to use a struct, add self to methods, and implement create_dir_if_not_exists

--- src/low_level/filesystem_manager.rs
+++ src/low_level/filesystem_manager.rs
@@ -1,15 +1,18 @@
 use std::fs;
 
-trait FileSystemManager {
-    fn list_files(dir: &str) -> Result<Vec<String>, String>;
-    fn read_file(path: &str) -> Result<String, String>;
-    fn copy_file(src: &str, dst: &str) -> Result<(), String>;
-    fn delete_file(path: &str) -> Result<(), String>;
-    fn exists(path: &str) -> Result<bool, String>;
+pub trait FileSystemManager {
+    fn list_files(&self,dir: &str) -> Result<Vec<String>, String>;
+    fn read_file(&self,path: &str) -> Result<String, String>;
+    fn copy_file(&self,src: &str, dst: &str) -> Result<(), String>;
+    fn delete_file(&self,path: &str) -> Result<(), String>;
+    fn exists(&self, path: &str) -> Result<bool, String>;
+    fn create_dir_if_not_exists(&self, path: &str) -> Result<(), String>;
 }
 
-impl FileSystemManager for FileSystemManager {
-    fn list_files(dir: &str) -> Result<Vec<String>, String> {
+pub struct DefaultFileSystemManager;
+
+impl FileSystemManager for DefaultFileSystemManager {
+    fn list_files(&self,dir: &str) -> Result<Vec<String>, String> {
         match fs::read_dir(dir) {
             Ok(entries) => {
                 let mut files = Vec::new();
@@ -23,31 +26,38 @@ impl FileSystemManager for FileSystemManager {
         }
     }
 
-    fn read_file(path: &str) -> Result<String, String> {
+    fn read_file(&self,path: &str) -> Result<String, String> {
         match fs::read(path) {
             Ok(content) => Ok(String::from_utf8(content).unwrap()),
             Err(error) => Err(error.to_string())
         }
     }
 
-    fn copy_file(src: &str, dst: &str) -> Result<(), String> {
+    fn copy_file(&self,src: &str, dst: &str) -> Result<(), String> {
         match fs::copy(src, dst) {
-            Ok(_) => Ok(),
+            Ok(_) => Ok(()),
             Err(error) => Err(error.to_string())
         }
     }
 
-    fn delete_file(path: &str) -> Result<(), String> {
+    fn delete_file(&self,path: &str) -> Result<(), String> {
         match fs::remove_file(path) {
-            Ok(_) => Ok(),
+            Ok(_) => Ok(()),
             Err(error) => Err(error.to_string())
         }
     }
 
-    fn exists(path: &str) -> Result<bool, String> {
+    fn exists(&self, path: &str) -> Result<bool, String> {
         match fs::exists(path) {
             Ok(exists) => Ok(exists),
             Err(error) => Err(error.to_string())
         }
+    }
+
+    fn create_dir_if_not_exists(&self, path: &str) -> Result<(), String> {
+        match fs::create_dir_all(path) {
+            Ok(_) => Ok(()),
+            Err(error) => Err(error.to_string())
+        }   
     }
 }
